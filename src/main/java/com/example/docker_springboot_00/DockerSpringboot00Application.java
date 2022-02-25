cc00ace65a48e353542ce9053eb5943e0eef68a7
bgr <jamasa97@icloud.com>
2022-02-25 14:44:01 +0900

    Initial commit

---
+++ src/main/java/com/example/docker_springboot_00/DockerSpringboot00Application.java
@@ -0,0 +1,13 @@
+package com.example.docker_springboot_00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringboot00Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerSpringboot00Application.class, args);
+    }
+
+}
